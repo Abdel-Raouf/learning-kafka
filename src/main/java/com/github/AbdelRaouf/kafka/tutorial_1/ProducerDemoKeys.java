b153822dfd4282af41d955f8afa6427d3b236150
abdelraouf <raoufmohamed@outlook.com>
2019-11-17 08:34:19 +0200

    making a java consumer that is subscribed to many topics

--- src/main/java/com/github/AbdelRaouf/kafka/tutorial_1/ProducerDemoKeys.java
+++ src/main/java/com/github/AbdelRaouf/kafka/tutorial_1/ProducerDemoKeys.java
@@ -6,11 +6,12 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.Properties;
+import java.util.concurrent.ExecutionException;
 
-public class ProducerDemoWithCallback {
+public class ProducerDemoKeys {
 
-    public static void main(String[] args) {
-        final Logger logger = LoggerFactory.getLogger(ProducerDemoWithCallback.class);
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+        final Logger logger = LoggerFactory.getLogger(ProducerDemoKeys.class);
         String bootstrapServers = "127.0.0.01:9092";
 //        create Producer Properties
         Properties properties = new Properties();
@@ -25,10 +26,27 @@ public class ProducerDemoWithCallback {
         for(int i=0; i<10; ++i) {
 
             //        create a producer record
-            ProducerRecord<String, String> producerRecord =
-                    new ProducerRecord<String, String>("first_topic", "Hello World From Abdel-Raouf " + Integer.toString(i));
 
-            //        send data
+            String topic = "first_topic";
+            String value = "hello world " + Integer.toString(i);
+            String key = "id_" + Integer.toString(i);
+
+            ProducerRecord<String, String> producerRecord =
+                    new ProducerRecord<String, String>(topic, key, value + Integer.toString(i));
+
+            logger.info("Key: " + key); //log the key
+            // id_0 is going to partition 1
+            // id_1 is going to partition 0
+            // id_2 is going to partition 2
+            // id_3 is going to partition 0
+            // id_4 is going to partition 2
+            // id_5 is going to partition 2
+            // id_6 is going to partition 0
+            // id_7 is going to partition 2
+            // id_8 is going to partition 1
+            // id_9 is going to partition 2
+
+            //        send data - asynchronous
             producer.send(producerRecord, new Callback() {
                 public void onCompletion(RecordMetadata recordMetadata, Exception e) {
                     //                execute every time a record is successfully sent or an exception is thrown
@@ -43,7 +61,7 @@ public class ProducerDemoWithCallback {
                         logger.error("Error while producing", e);
                     }
                 }
-            });
+            }).get(); // block the .send() to make it synchronous - don't do this in production
         }
 //        flush data
         producer.flush();
@@ -51,4 +69,5 @@ public class ProducerDemoWithCallback {
 //        flush and close producer
         producer.close();
     }
+
 }
